0a62f50b463c4ecea2847b4c288b6d3f2e3049db
Slim Nejmaoui <85970006+Slimnajmaoui@users.noreply.github.com>
2024-10-29 13:16:00 +0100

    Update DemoApplicationTests.java 5

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,12 +1,12 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.ActiveProfiles;
+
 
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@ActiveProfiles("test")
+
 class DemoApplicationTests {
 
 	@Test
