c40c4e0df079543d799abc11a859664df8d02a31
Slim Nejmaoui <85970006+Slimnajmaoui@users.noreply.github.com>
2024-11-10 20:53:22 +0100

    Update DemoApplication.java 10

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.example.demo.repository")  // Spécifie où Spring doit chercher les repositories MongoDB
+@ComponentScan(basePackages = "com.example.demo.model")  // Spécifie où Spring doit chercher les entités MongoDB
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
